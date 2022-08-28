4bc541d8df90f72914659bcab40e73e8646a87e4
mahmud139 <mahmudulhasan139139@gmail.com>
2022-08-28 19:26:24 +0600

    added two handler

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on localhost:8080")
 	err := http.ListenAndServe("localhost:8080", mux)
@@ -16,4 +18,12 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
 }
