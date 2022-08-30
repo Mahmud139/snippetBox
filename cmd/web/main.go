7facbc84217cb2a5e2ecf3d632c892e7c8229b82
mahmud139 <mahmudulhasan139139@gmail.com>
2022-08-30 19:45:21 +0600

    adding fileserver

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,9 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileserver := http.FileServer(http.Dir("M:/Projects/snippetbox/ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+
 	log.Println("Starting server on localhost:8080")
 	err := http.ListenAndServe("localhost:8080", mux)
 	log.Fatal(err)
