4050c5557c6b313aad124947f65b581d85c37dd8
mahmud139 <mahmudulhasan139139@gmail.com>
2022-08-31 09:00:54 +0600

    added laveled logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,16 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	addr := flag.String("addr", "localhost:8080", "HTTP Netword Address")
 	flag.Parse()
 	
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -18,8 +22,8 @@ func main() {
 	fileserver := http.FileServer(http.Dir("M:/Projects/snippetbox/ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
 
-	log.Printf("Starting server on %s\n",*addr)
+	infoLog.Printf("Starting server on %s\n",*addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
 
