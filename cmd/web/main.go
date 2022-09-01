ff4f74305353c8d8a58de60870c74d5a1d06d39d
mahmud139 <mahmudulhasan139139@gmail.com>
2022-09-01 11:14:14 +0600

    added database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,6 +17,7 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", "localhost:8080", "HTTP Netword Address")
+	dsn := flag.String("dsn", "web:mahmud@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 	
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -24,6 +28,12 @@ func main() {
 		infoLog: infoLog,
 	}
 
+	db, err := OpenDB(*dsn)																																														
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
 	server := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
@@ -31,7 +41,19 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s\n",*addr)
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func OpenDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
