e33e28960c1673ade44e63efd761067ae621e67c
mahmud139 <mahmudulhasan139139@gmail.com>
2022-09-01 16:06:59 +0600

    update database models

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Mahmud139/snippetbox/pkg/models/mysql"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	errorLog *log.Logger
 	infoLog *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
@@ -23,16 +25,17 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	app := &application{
-		errorLog: errorLog,
-		infoLog: infoLog,
-	}
-
 	db, err := OpenDB(*dsn)																																														
 	if err != nil {
 		errorLog.Fatal(err)
 	}
 	defer db.Close()
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
+	}
 
 	server := &http.Server{
 		Addr: *addr,
