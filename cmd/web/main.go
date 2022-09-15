55eba18f77c81f9257e1bef65a9c1ad09213887b
mahmud139 <mahmudulhasan139139@gmail.com>
2022-09-15 20:09:35 +0600

    formatting the codebase

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -17,7 +17,7 @@ import (
 type application struct {
 	errorLog      *log.Logger
 	infoLog       *log.Logger
-	session *sessions.Session
+	session       *sessions.Session
 	snippets      *mysql.SnippetModel
 	templateCache map[string]*template.Template
 }
@@ -48,7 +48,7 @@ func main() {
 	app := &application{
 		errorLog:      errorLog,
 		infoLog:       infoLog,
-		session: session,
+		session:       session,
 		snippets:      &mysql.SnippetModel{DB: db},
 		templateCache: templateCache,
 	}
