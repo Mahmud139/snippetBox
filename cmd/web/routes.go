6f3e5e9a9c91b7021f479447472122aa18b18c9f
mahmud139 <mahmudulhasan139139@gmail.com>
2022-09-19 19:29:01 +0600

    updated routers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,7 +17,11 @@ func (app *application) routes() http.Handler {
 	mux.Post("/snippet/create", dynamicMiddleware.ThenFunc(app.createSnippet))
 	mux.Get("/snippet/:id", dynamicMiddleware.ThenFunc(app.showSnippet))
 
-	mux.Get("/user/signup", http.HandleFunc(app.))
+	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
+	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
+	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
+	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
+	mux.Post("/user/logout", http.HandlerFunc(app.logoutUser))
 
 	fileserver := http.FileServer(http.Dir("M:/Projects/snippetbox/ui/static/"))
 	mux.Get("/static/", http.StripPrefix("/static", fileserver))
