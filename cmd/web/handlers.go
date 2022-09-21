78a5fe3b3282d03193b517bf46f8e5d3b0c2be08
mahmud139 <mahmudulhasan139139@gmail.com>
2022-09-21 10:07:12 +0600

    updated handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -84,7 +84,9 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 
 
 func (app *application) signupUserForm(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, "signup.page.tmpl", nil)
+	app.render(w, r, "signup.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) signupUser(w http.ResponseWriter, r *http.Request) {
