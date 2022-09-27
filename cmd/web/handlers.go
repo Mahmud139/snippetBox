d9b8fc1965754aa26d028ef02a9b97b9c0fd14ce
mahmud139 <mahmudulhasan139139@gmail.com>
2022-09-27 19:19:34 +0600

    updated handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -181,7 +181,27 @@ func (app *application) changePasswordForm(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) changePassword(w http.ResponseWriter, r *http.Request) {
-	
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	form := forms.New(r.PostForm)
+	form.Required("currentPassword", "newPassword", "newPasswordConfirmation")
+	form.MinLength("newPassword", 8)
+	if form.Get("newPassword") != form.Get("newPasswordConfirmation") {
+		form.Errors.Add("newPasswordConfirmation", "Passwords do not match")
+	}
+
+	if !form.Valid() {
+		app.render(w, r, "changePassword.page.tmpl", &templateData{
+			Form: form,
+		})
+		return
+	}
+
+	fmt.Fprint(w, "all is ok")
 }
 
 func (app *application) about(w http.ResponseWriter, r *http.Request) {
